a2013911bb09f4646229a8c5e7c6b98f4c56e804
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2022-02-07 21:11:24 +0300

    add stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,41 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Gebindo'),
         centerTitle: true,
+        backgroundColor: Colors.green[300],
       ),
       body: const Center(
-        child: Text('Yo bruh'),
+        child: Text(
+          'Yo bruh siet... \n changes are reflecting!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            color: Colors.teal,
+            fontFamily: 'IndieFLower',
+          ),
+        ),
       ),
-      // ignore: prefer_const_constructors
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Text('siet'),
+        backgroundColor: Colors.green[300],
       ),
-    ),
-  ));
+    );
+  }
 }
